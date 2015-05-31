df7b7a7706b0a2c68981d69039b61f93d7dfa6a1
Chris Greenaway <chris@rubbem.org>
2015-05-31 22:21:49 +0100

    CG: Get a plain GTK window showing

--- rubbem/src/main.rs
+++ rubbem/src/main.rs
@@ -1,3 +1,26 @@
+extern crate gtk;
+
+use gtk::traits::*;
+
 fn main() {
-    println!("Hello, world!");
+    if !gtk::init_check() {
+        println!("Cannot start because GTK is not working / available");
+        return;
+    }
+
+    match gtk::Window::new(gtk::WindowType::TopLevel) {
+        None => println!("Unable to create a GTK window."),
+        Some(window) => {
+            window.set_title("Rubbem");
+            window.set_window_position(gtk::WindowPosition::Center);
+
+            window.connect_delete_event(|_, _| {
+                gtk::main_quit();
+                gtk::signal::Inhibit(false)
+            });
+
+            window.show_all();
+            gtk::main();
+        }
+    }
 }
